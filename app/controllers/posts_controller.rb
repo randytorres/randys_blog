1830c24da8e686c6c83ba6317a540340a989d0ab
Randy Torres <torresr14@gmail.com>
2015-09-30 20:22:25 -0700

    Simplified PostController by removing references to JSON
    We  also deleted the json Jb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+
   end
 
   # GET /posts/new
@@ -22,42 +23,32 @@ class PostsController < ApplicationController
   end
 
   # POST /posts
-  # POST /posts.json
+ 
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to @post, notice: "Post was sucessfully created."
+    else
+      render action: 'new'
     end
   end
 
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+        if @post.update(post_params)
+          redirect_to @post, notice: 'Post was successfully updated.' 
+        else 
+          render action: 'edit'
+        end
   end
 
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
+    redirect_to posts_url
     end
   end
 
